9cec5b37fc541d988e3030317edd402d5833be88
fengmk2 <fengmk2@gmail.com>
2015-02-27 13:53:12 +0800

    add more json content-type: application/vnd.api+json and application/csp-report

--- test/any.js
+++ test/any.js
@@ -56,23 +56,31 @@ describe('parse(req, opts)', function(){
     })
   })
 
-  describe('with valid json-patch content-type', function(){
-    it('should parse', function(done){
-      var app = koa();
+  describe('with know json content-type', function(){
+    var app = koa();
 
-      app.use(function *(){
-        var body = yield parse(this);
-        body.should.eql([{op: 'replace', path: '/foo', value:'bar'}]);
-        done();
-      });
+    app.use(function *(){
+      this.body = yield parse(this);
+    });
 
+    it('should parse application/json-patch+json', function(done){
       request(app.listen())
       .post('/')
       .type('application/json-patch+json')
       .send(JSON.stringify([{op: 'replace', path: '/foo', value:'bar'}]))
-      .end(function(){});
-    })
-  })
+      .expect(200)
+      .expect([{op: 'replace', path: '/foo', value:'bar'}], done);
+    });
+
+    it('should parse application/vnd.api+json', function(done){
+      request(app.listen())
+      .post('/')
+      .type('application/vnd.api+json')
+      .send(JSON.stringify({posts: "1"}))
+      .expect(200)
+      .expect({posts: "1"}, done);
+    });
+  });
 
 
   describe('with missing content-type', function(){
