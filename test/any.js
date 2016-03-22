b00173441920ee65b1691cd8fd18d51a9d043107
Blake Miner <miner.blake@gmail.com>
2016-03-22 13:08:15 -0400

    feat: Added support for request body inflation
    closes #38

--- test/any.js
+++ test/any.js
@@ -2,6 +2,7 @@
 var request = require('supertest');
 var parse = require('..');
 var koa = require('koa');
+var zlib = require('zlib');
 
 describe('parse(req, opts)', function(){
   describe('with valid form body', function(){
@@ -147,4 +148,57 @@ describe('parse(req, opts)', function(){
       .expect(415, 'Unsupported Media Type', done);
     })
   })
+
+  describe('with content-encoding', function(){
+    it('should inflate gzip', function(done){
+      var app = koa();
+      var json = JSON.stringify({ foo: 'bar' });
+
+      app.use(function *(){
+        var body = yield parse(this);
+        body.should.eql({ foo: 'bar' });
+        done();
+      });
+
+      var req = request(app.listen())
+        .post('/')
+        .type('json')
+        .set('Content-Encoding', 'gzip');
+      req.write(zlib.gzipSync(json));
+      req.end(function(){});
+    })
+    it('should inflate deflate', function(done){
+      var app = koa();
+      var json = JSON.stringify({ foo: 'bar' });
+
+      app.use(function *(){
+        var body = yield parse(this);
+        body.should.eql({ foo: 'bar' });
+        done();
+      });
+
+      var req = request(app.listen())
+        .post('/')
+        .type('json')
+        .set('Content-Encoding', 'deflate');
+      req.write(zlib.deflateSync(json));
+      req.end(function(){});
+    })
+    it('should pass-through identity', function(done){
+      var app = koa();
+
+      app.use(function *(){
+        var body = yield parse(this);
+        body.should.eql({ foo: 'bar' });
+        done();
+      });
+
+      request(app.listen())
+      .post('/')
+      .set('Content-Encoding', 'identity')
+      .send({ foo: 'bar' })
+      .end(function(){});
+    })
+  })
+
 })
