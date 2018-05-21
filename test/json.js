92ce318a31da6627b79dc88cd78fbe013c688011
popomore <sakura9515@gmail.com>
2018-05-21 15:30:04 +0800

    eslint

--- test/json.js
+++ test/json.js
@@ -1,84 +1,85 @@
+'use strict';
 
-var request = require('supertest');
-var parse = require('..');
-var koa = require('koa');
+const request = require('supertest');
+const parse = require('..');
+const koa = require('koa');
 
-describe('parse.json(req, opts)', function(){
-  describe('with valid json', function(){
-    it('should parse', function(done){
-      var app = koa();
+describe('parse.json(req, opts)', function() {
+  describe('with valid json', function() {
+    it('should parse', function(done) {
+      const app = koa();
 
-      app.use(function *(){
-        var body = yield parse.json(this);
+      app.use(function* () {
+        const body = yield parse.json(this);
         body.should.eql({ foo: 'bar' });
         done();
       });
 
       request(app.listen())
-      .post('/')
-      .send({ foo: 'bar' })
-      .end(function(){});
-    })
-  })
+        .post('/')
+        .send({ foo: 'bar' })
+        .end(function() {});
+    });
+  });
 
   describe('with invalid content encoding', function() {
     it('should throw 415', function(done) {
-      var app = koa();
+      const app = koa();
 
-      app.use(function *(){
-        var body = yield parse.json(this);
+      app.use(function* () {
+        const body = yield parse.json(this);
         body.foo.bar.should.equal('baz');
         this.status = 200;
       });
 
       request(app.listen())
-      .post('/')
-      .type('json')
-      .set('content-encoding', 'invalid')
-      .send({ foo: { bar: 'baz' }})
-      .expect(415, done);
-    })
-  })
+        .post('/')
+        .type('json')
+        .set('content-encoding', 'invalid')
+        .send({ foo: { bar: 'baz' } })
+        .expect(415, done);
+    });
+  });
 
-  describe('with content-length zero', function(){
-    describe('and strict === false', function(){
+  describe('with content-length zero', function() {
+    describe('and strict === false', function() {
       it('should return null', function(done) {
-        var app = koa();
+        const app = koa();
 
-        app.use(function *() {
-          var body = yield parse.json(this, {strict: false});
+        app.use(function* () {
+          const body = yield parse.json(this, { strict: false });
           body.should.equal('');
           done();
         });
         request(app.listen())
-        .post('/')
-        .set('content-length', 0)
-        .end(function(){});
-      })
-    })
+          .post('/')
+          .set('content-length', 0)
+          .end(function() {});
+      });
+    });
 
-    describe('and strict === true', function(){
+    describe('and strict === true', function() {
       it('should return null', function(done) {
-        var app = koa();
+        const app = koa();
 
-        app.use(function *() {
-          var body = yield parse.json(this);
+        app.use(function* () {
+          const body = yield parse.json(this);
           body.should.eql({});
           done();
         });
         request(app.listen())
-        .post('/')
-        .set('content-length', 0)
-        .end(function(){});
-      })
-    })
-  })
+          .post('/')
+          .set('content-length', 0)
+          .end(function() {});
+      });
+    });
+  });
 
-  describe('with invalid json', function(){
-    it('should parse error', function(done){
-      var app = koa();
+  describe('with invalid json', function() {
+    it('should parse error', function(done) {
+      const app = koa();
 
-      app.use(function *(){
+      app.use(function* () {
         try {
           yield parse.json(this);
         } catch (err) {
@@ -89,39 +90,39 @@ describe('parse.json(req, opts)', function(){
       });
 
       request(app.listen())
-      .post('/')
-      .set('content-type', 'application/json')
-      .send('{"foo": "bar')
-      .end(function(){});
-    })
-  })
+        .post('/')
+        .set('content-type', 'application/json')
+        .send('{"foo": "bar')
+        .end(function() {});
+    });
+  });
 
-  describe('with non-object json', function(){
-    describe('and strict === false', function(){
-      it('should parse', function(done){
-        var app = koa();
+  describe('with non-object json', function() {
+    describe('and strict === false', function() {
+      it('should parse', function(done) {
+        const app = koa();
 
-        app.use(function *(){
-          var body = yield parse.json(this, {strict: false});
+        app.use(function* () {
+          const body = yield parse.json(this, { strict: false });
           body.should.equal('foo');
           done();
         });
 
         request(app.listen())
-        .post('/')
-        .set('content-type', 'application/json')
-        .send('"foo"')
-        .end(function(){});
-      })
-    })
+          .post('/')
+          .set('content-type', 'application/json')
+          .send('"foo"')
+          .end(function() {});
+      });
+    });
 
-    describe('and strict === true', function(){
-      it('should parse', function(done){
-        var app = koa();
+    describe('and strict === true', function() {
+      it('should parse', function(done) {
+        const app = koa();
 
-        app.use(function *(){
+        app.use(function* () {
           try {
-            yield parse.json(this, {strict: true});
+            yield parse.json(this, { strict: true });
           } catch (err) {
             err.status.should.equal(400);
             err.body.should.equal('"foo"');
@@ -131,28 +132,28 @@ describe('parse.json(req, opts)', function(){
         });
 
         request(app.listen())
-        .post('/')
-        .set('content-type', 'application/json')
-        .send('"foo"')
-        .end(function(){});
-      })
-    })
-  })
+          .post('/')
+          .set('content-type', 'application/json')
+          .send('"foo"')
+          .end(function() {});
+      });
+    });
+  });
 
-  describe('returnRawBody', function(){
-    it('should return raw body when opts.returnRawBody = true', function(done){
-      var app = koa();
+  describe('returnRawBody', function() {
+    it('should return raw body when opts.returnRawBody = true', function(done) {
+      const app = koa();
 
-      app.use(function *(){
+      app.use(function* () {
         this.body = yield parse.json(this, { returnRawBody: true });
       });
 
       request(app.listen())
-      .post('/')
-      .type('json')
-      .send({ foo: 'bar' })
-      .expect({ parsed: { foo: 'bar' }, raw: '{"foo":"bar"}' })
-      .expect(200, done);
+        .post('/')
+        .type('json')
+        .send({ foo: 'bar' })
+        .expect({ parsed: { foo: 'bar' }, raw: '{"foo":"bar"}' })
+        .expect(200, done);
     });
-  })
-})
+  });
+});
--- test/text.js
+++ test/text.js
@@ -1,65 +1,66 @@
+'use strict';
 
-var request = require('supertest');
-var parse = require('..');
-var koa = require('koa');
-var Buffer = require('buffer').Buffer;
+const request = require('supertest');
+const parse = require('..');
+const koa = require('koa');
+const Buffer = require('buffer').Buffer;
 
-describe('parse.text(req, opts)', function(){
-  describe('with valid str', function(){
-    it('should parse', function(done){
-      var app = koa();
+describe('parse.text(req, opts)', function() {
+  describe('with valid str', function() {
+    it('should parse', function(done) {
+      const app = koa();
 
-      app.use(function *(){
+      app.use(function* () {
         this.body = yield parse.text(this);
       });
 
       request(app.listen())
-      .post('/')
-      .send('Hello World!')
-      .expect(200)
-      .expect('Hello World!', done);
+        .post('/')
+        .send('Hello World!')
+        .expect(200)
+        .expect('Hello World!', done);
     });
   });
 
   describe('with invalid content encoding', function() {
     it('should throw 415', function(done) {
-      var app = koa();
+      const app = koa();
 
-      app.use(function *(){
-        var body = yield parse.text(this);
+      app.use(function* () {
+        yield parse.text(this);
         this.status = 200;
       });
 
       request(app.listen())
-      .post('/')
-      .set('content-encoding', 'invalid')
-      .send('Hello World!')
-      .expect(415, done);
-    })
-  })
+        .post('/')
+        .set('content-encoding', 'invalid')
+        .send('Hello World!')
+        .expect(415, done);
+    });
+  });
 
-  describe('returnRawBody', function(){
-    it('should return raw body when opts.returnRawBody = true', function(done){
-      var app = koa();
+  describe('returnRawBody', function() {
+    it('should return raw body when opts.returnRawBody = true', function(done) {
+      const app = koa();
 
-      app.use(function *(){
+      app.use(function* () {
         this.body = yield parse.text(this, { returnRawBody: true });
       });
 
       request(app.listen())
-      .post('/')
-      .send('Hello World!')
-      .expect({ parsed: 'Hello World!', raw: 'Hello World!' })
-      .expect(200, done);
+        .post('/')
+        .send('Hello World!')
+        .expect({ parsed: 'Hello World!', raw: 'Hello World!' })
+        .expect(200, done);
     });
-  })
+  });
 
-  describe('use no encoding', function(){
-    it('should return raw body when opts.returnRawBody = true', function(done){
-      var app = koa();
+  describe('use no encoding', function() {
+    it('should return raw body when opts.returnRawBody = true', function(done) {
+      const app = koa();
 
-      app.use(function *(){
-        const requestBody = yield parse.text(this, {encoding: false});
+      app.use(function* () {
+        const requestBody = yield parse.text(this, { encoding: false });
         this.body = { isBuffer: Buffer.isBuffer(requestBody) };
       });
 
@@ -69,5 +70,5 @@ describe('parse.text(req, opts)', function(){
         .expect({ isBuffer: true })
         .expect(200, done);
     });
-  })
+  });
 });
